11a5c9c92ecf0a653387a90f2ae8b4caf3cbacf2
Arcka <valerie.lecoeur75@gmail.com>
2022-08-17 22:13:43 +0200

    ajout et suppression des taches avec persistance des données en local storage

--- src/app/formulaire/formulaire.component.ts
+++ src/app/formulaire/formulaire.component.ts
@@ -16,24 +16,61 @@ export class FormulaireComponent implements OnInit {
     { description: 'Revoir la documentation d\'Angular ', done: false },
   ];
 
-  get items() {    
-      return this.allItems;        
+  get items() {   
+    if(localStorage.getItem('task')!= null ){      
+      let newItems:any = localStorage.getItem('task');
+      let result = JSON.parse(newItems);
+      console.log(result);
+     return JSON.parse(newItems);
+
+    }else{
+      localStorage.setItem('task',JSON.stringify(this.allItems));
+      let newItems:any = localStorage.getItem('task');
+      let result = JSON.parse(newItems);
+      console.log("nulle " +result);
+     return JSON.parse(newItems);
+
+    }
+
+    
   }
 
   addItem(description: string) {
-    this.allItems.unshift({
+    let newItems:any = localStorage.getItem('task');
+    let result = JSON.parse(newItems);
+    result.unshift({
       description,
       done: false
-    });
+    });    
+    localStorage.setItem('task',JSON.stringify(result));
+    
+    
   }
 
   deleteItem(index :any){
-    this.items.splice(index, 1);
+    let newItems:any = localStorage.getItem('task');
+    let result = JSON.parse(newItems);
+    result.splice(index, 1);
+    localStorage.setItem('task',JSON.stringify(result));
+
   }
 
-  constructor() { }  
+  constructor( ) { }  
 
   ngOnInit(): void {
+
   }
 
 }
+/*
+get items() {   
+  localStorage.setItem('task',JSON.stringify(this.allItems));    
+   return localStorage.getItem('task');
+}
+
+addItem(description: string) {
+ this.allItems.unshift({
+   description,
+   done: false
+ });
+}*/
