7b95d6b25523ce1ade0a9dd411fef930c054e22b
Arcka <valerie.lecoeur75@gmail.com>
2022-08-17 14:26:06 +0200

    Mise en place de l'interface graphique (non-fonctionnelle)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormulaireComponent } from './formulaire/formulaire.component';
+import { TasklistComponent } from './tasklist/tasklist.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormulaireComponent,
+    TasklistComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
