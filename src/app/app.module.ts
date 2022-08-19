720791912507eda120e8bfe5b945b2071e924bd7
Arcka <valerie.lecoeur75@gmail.com>
2022-08-19 14:19:44 +0200

    modification de tache mise en place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { FormulaireComponent } from './formulaire/formulaire.component';
-import { TasklistComponent } from './tasklist/tasklist.component';
+
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
@@ -12,12 +10,14 @@ import { FooterComponent } from './footer/footer.component';
   declarations: [
     AppComponent,
     FormulaireComponent,
-    TasklistComponent,
+    
     HeaderComponent,
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/formulaire/formulaire.component.ts
+++ src/app/formulaire/formulaire.component.ts
@@ -8,26 +8,29 @@ import { Component, OnInit } from '@angular/core';
 
 export class FormulaireComponent implements OnInit {
  
-
+  description=0;
+  condition = false;
+  indexRecupere=0;
   allItems = [
     { description: 'Etudier Angular', done: true },
     { description: 'Faire un premier projet Angular', done: false },
     { description: 'Reviser l\'anglais', done: false },
     { description: 'Revoir la documentation d\'Angular ', done: false },
   ];
+  
 
   get items() {   
     if(localStorage.getItem('task')!= null ){      
       let newItems:any = localStorage.getItem('task');
       let result = JSON.parse(newItems);
-      console.log(result);
+      
      return JSON.parse(newItems);
 
     }else{
       localStorage.setItem('task',JSON.stringify(this.allItems));
       let newItems:any = localStorage.getItem('task');
       let result = JSON.parse(newItems);
-      console.log("nulle " +result);
+      
      return JSON.parse(newItems);
 
     }
@@ -54,6 +57,25 @@ export class FormulaireComponent implements OnInit {
     localStorage.setItem('task',JSON.stringify(result));
 
   }
+  recupItem(index :any){
+    let newItems:any = localStorage.getItem('task');
+    let result = JSON.parse(newItems);
+    this.indexRecupere = index;
+    this.description = result[index].description;
+    console.log(result);
+    this.condition = true;
+  }
+  modifyItem(description:string){
+    let newItems:any = localStorage.getItem('task');
+    let result = JSON.parse(newItems);
+    result[this.indexRecupere]={
+      description,
+      done: false
+    };    
+    localStorage.setItem('task',JSON.stringify(result));
+    this.condition=false;
+
+  }
 
   constructor( ) { }  
 
@@ -62,15 +84,3 @@ export class FormulaireComponent implements OnInit {
   }
 
 }
-/*
-get items() {   
-  localStorage.setItem('task',JSON.stringify(this.allItems));    
-   return localStorage.getItem('task');
-}
-
-addItem(description: string) {
- this.allItems.unshift({
-   description,
-   done: false
- });
-}*/
